c549c64b176d3f36d40708f6e94fce7a5707d057
gyeongsu lim <05dos1211@naver.com>
2025-07-17 17:38:54 +0900

    20936 수정 중 왜 틀렸는지 도저히 모르겠음

--- swea/20936_상자정렬하기.py
+++ swea/20936_상자정렬하기.py
@@ -1,48 +1,58 @@
 t = int(input())
 
-n = int(input())
-nums = list(map(int, input().split()))
+for _ in range(t):
+    
+    n = int(input())
+    nums = list(map(int, input().split()))
+    
+    good = [i+1 == nums[i] for i in range(n)]
+    if n == sum(good):
+        print(0)
+        print()
 
-# nums = [4,5,6,3,1,2,7,8,9]
-nums.append(999) #빈칸 추가
+    else:
+        try :
+            biggest_wrong = max([nums[i] for i in range(n) if not good[i]])
+        except:
+            biggest_wrong = n
 
-# nums = [9,5,6,3,1,2,7,8,4,999] #1
-# nums = [999,5,6,3,1,2,7,8,4,9] #5
-# nums = [1,5,6,3,999,2,7,8,4,9]
-# nums = [1,999,6,3,5,2,7,8,4,9]
-# nums = [1,2,6,3,5,999,7,8,4,9]
-# nums = [1,2,999,3,5,6,7,8,4,9]
-# nums = [1,2,3,999,5,6,7,8,4,9]
-# nums = [1,2,3,4,5,6,7,8,999,9]
-# nums = [1,2,3,4,5,6,7,8,9,999]
+        log = []
+                
+        nums.append(999) #빈칸 추가
+        idx2 = nums.index(biggest_wrong) # 제자리가 아닌 상자 중 가장 큰 상자의 인덱스
 
-log = []
-# nums =[1,2,3]
-#처음꺼 바꾸는거 만약 제일 큰숫자가 제자리에 있다면? 똑같이 동작하긴 하는데
-idx2 = nums.index(999)
-idx1 = nums.index(idx2)
+        #가장 큰 상자 맨 뒤로 빼기
+        nums[idx2] = 999 
+        nums[-1] = biggest_wrong
 
-nums[idx1] = 999
-nums[idx2] = idx2
+        #제일 큰 숫자가 들어있던 칸의 번호
+        log.append(idx2+1)
 
-log.append(idx1+1)
+        while nums[-1] != 999:
 
-nums
-log
-#종료까지 코드
-while nums[-1] != 999:
+            idx3 = nums.index(999)+1 #빈칸의 상자 번호
+            idx4 = nums.index(idx3) #빈칸 번호와 같은 상자가 위치한 칸의 번호
 
-    idx3 = nums.index(999)+1 #왜 1을 더하는가 +1한 위치를 찾아야하니까
-    idx4 = nums.index(idx3)
+            nums[idx4] = 999 #상자 이동해서 빈칸이 됨
+            nums[idx3-1] = idx3# 빈칸에 알맞은 상자를 넣음
 
-    nums[idx4] = 999
-    nums[idx3-1] = idx3 # 왜 1을 빼는가 변경되야할 위치는 -1해야 정확하니까
+            log.append(idx4 + 1) #이동한 상자의 칸 번호
 
-    log.append(idx4 + 1)
+        print(len(log))
+        for i in log:
+            print(i, end=' ')
 
-    nums
-    log
+# test case
 
-print(len(log))
-for i in log:
-    print(i, end=' ')
+#통과
+# 3
+# 1 2 3
+
+#통과
+# 4
+# 2 3 4 1
+
+#통과
+# 6
+# 1 2 3 5 4 6
+
