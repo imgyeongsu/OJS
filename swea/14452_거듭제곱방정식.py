faf27d1feea3b6b9c60982d7b0a07a317fac8cea
gyeongsu lim <05dos1211@naver.com>
2025-07-18 17:52:55 +0900

    14452 진행중

--- swea/14452_거듭제곱방정식.py
+++ swea/14452_거듭제곱방정식.py
@@ -1,10 +1,25 @@
 T = int(input())
-
+limit = 1_000_000_000
 X, N, Y, M = map(int, input().split())
 
 # i) x와 y가 1인경우
-case1 = n * m
+case1 = N * M
 # ii) n과 m이 1인경우
-case2 = min(x, y)
+case2 = min(X, Y)
 # iii) x^k = y 꼴로 표현되는 경우
+
+
+# k = 2일때 
+case3 = 0
+for xi in range(2, int(Y ** 0.5) + 1):
+    case3 = min(N/2, M)
+    
+# iii-i) x는 최대 y의 sqrt다 
+
 # ix) x=y^k 꼴로 표현되는 경우
+
+Y = 101
+int(Y ** 0.5)
+
+
+a ** 3 = 3 ** a
