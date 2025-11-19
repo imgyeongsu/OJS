6c9f2f9bc6aa5a140983a6d3e95a93759716cece
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-20 08:55:48 +0900

    Title: A부터 B까지 짝수의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251120/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.cpp
+++ 251120/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.cpp
@@ -6,7 +6,7 @@ int main() {
     int a , b;
     cin >> a >> b;
     int hap = 0;
-    for (int i = a; i < b +1; i+=2){
+    for (int i = a; i < b+1; i++){
         if (i % 2 == 0) {
             hap += i;
         }
