88717cfca0725185b96bb346feb814da0d8a0782
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-19 16:26:44 +0900

    Title: 제곱하여 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251119/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    int n;
+    for (int i=0; i < N; i++){
+        cin >> n;
+        cout << n * n << ' ';
+    }
+    return 0;
+}
