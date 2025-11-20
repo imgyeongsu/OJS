50362b5d987fa4aa2d6fc6ab112fd470e64ba690
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-20 10:06:20 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251120/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    while (true) {
+        cin >> n;
+        if (n){
+            cout << n << '\n';
+        } else {
+            break;
+        }
+    }
+    return 0;
+}
