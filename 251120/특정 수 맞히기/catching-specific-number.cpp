a2dc8a8f7881c16e9ce6a62c189466cd5473c980
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-20 11:14:37 +0900

    Title: 특정 수 맞히기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251120/특정 수 맞히기/catching-specific-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    while (true) {
+        cin >> n;
+        if (n < 25){
+            cout << "Higher\n";
+        } else if (n > 25) {
+            cout << "Lower\n";
+        } else {
+            cout << "Good";
+            break;
+        }
+    }
+    return 0;
+}
