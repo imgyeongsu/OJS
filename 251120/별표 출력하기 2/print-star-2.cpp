fdf8dbcd74dd7c7c98520640929b4fa7431d8ea9
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-20 21:19:30 +0900

    Title: 별표 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251120/별표 출력하기 2/print-star-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    for (int i = N; i > 0; i--){
+        for (int j=0; j <i; j++){
+            cout << "* ";
+        }
+        cout << '\n';
+    } 
+    return 0;
+}
