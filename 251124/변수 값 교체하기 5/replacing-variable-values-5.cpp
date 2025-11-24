7f6b0a8d483a017da7d19716780ff1237bf0dbf5
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-24 14:04:08 +0900

    Title: 변수 값 교체하기 5, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251124/변수 값 교체하기 5/replacing-variable-values-5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 2;
+    int b = 6;
+    a = b;
+    cout << a << '\n' << b;
+    
+
+    return 0;
+}
