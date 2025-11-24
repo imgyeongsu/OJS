37427c419ff245c1695509a8e8526d9a95a5b727
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-24 12:32:09 +0900

    Title: 두줄 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251124/두줄 출력/print-two-sentences-introduce.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string msg1 = "Hello students!";
+    string msg2 = "Welcome to LeebrosCode!";
+
+    cout << msg1 << '\n' << msg2;
+    return 0;
+}
