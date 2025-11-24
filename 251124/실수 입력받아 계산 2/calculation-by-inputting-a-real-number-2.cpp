0fbb220b84e1bd16ddf62a07efe73373a6df82d9
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-24 17:59:00 +0900

    Title: 실수 입력받아 계산 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251124/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a;
+    cin >> a;
+    
+    cout.fixed;
+    cout.precision(3);
+    cout << a + 1.5;
+    return 0;
+}
