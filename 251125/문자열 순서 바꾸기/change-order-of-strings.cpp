ecf648848f9ae7273d3f2db5946e9da38a2f1015
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-25 10:55:54 +0900

    Title: 문자열 순서 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251125/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string s1, s2;
+    cin >> s1 >> s2;
+    swap(s1, s2);
+    cout << s1 << '\n' << s2;
+    return 0;
+}
