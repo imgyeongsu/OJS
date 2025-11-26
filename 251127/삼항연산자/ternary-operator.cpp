c2cb78eed568bd9cacaf35ea9aa6fc04d41b534e
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-27 08:47:08 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251127/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    string res = a == 100? "pass" : "failure";
+    cout << res;
+    return 0;
+}
