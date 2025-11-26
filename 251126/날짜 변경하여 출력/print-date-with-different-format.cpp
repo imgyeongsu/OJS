3b2cb05d2064e62abb44124a671808b896645fa6
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-26 16:02:33 +0900

    Title: 날짜 변경하여 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251126/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int y, m, d;
+    char c;
+    cin >> y >> c >> m >> c >> d;
+    cout << m << '-' << d << '-' << y;
+    return 0;
+}
