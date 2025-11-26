771ab2a2592ce46d59d80bc43998db8871930bf3
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-26 16:41:29 +0900

    Title: 정사각형의 넓이, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251126/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    cout << n * n;
+    if (n < 5){
+        cout << "\ntiny";
+    }
+    return 0;
+}
