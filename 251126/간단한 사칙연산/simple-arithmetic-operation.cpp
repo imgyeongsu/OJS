25124d533361acbcc768b5b5459202bd5a910f27
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-26 16:05:24 +0900

    Title: 간단한 사칙연산, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251126/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    char c = '\n';
+    cin >> a >> b;
+    cout << a + b << c << a - b << c <<  a / b << c << a % b;
+    return 0;
+}
