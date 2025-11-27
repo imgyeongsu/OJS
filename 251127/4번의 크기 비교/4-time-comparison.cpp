ad09c466dadb445462a3ce058003ef1128481775
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-27 17:52:01 +0900

    Title: 4번의 크기 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251127/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    char g = '\n';
+    cout << (a > b) << g;
+    cout << (a > c) << g;
+    cout << (a > d) << g;
+    cout << (a > e) << g;
+    return 0;
+}
