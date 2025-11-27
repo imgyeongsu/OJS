cb0f0222cc04f7b473dc1ac1f6a6ef64e560244f
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-27 17:49:02 +0900

    Title: 비교 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251127/비교 연산/comparison-operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    char c = '\n';
+    cin >> a >> b;
+    cout << (a >=b ) << c;
+    cout << (a > b ) << c;
+    cout << (b >= a) << c;
+    cout << (b > a) << c;
+    cout << (a==b) << c;
+    cout << (a!=b);
+
+    return 0;
+}
