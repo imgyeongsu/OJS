2701cca214d0109444ed1721b163413a08ab94e8
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-28 22:41:19 +0900

    Title: 세 수의 중간값, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251128/세 수의 중간값/median-of-three-numbers.cpp
+++ 251128/세 수의 중간값/median-of-three-numbers.cpp
@@ -5,7 +5,7 @@ int main() {
     // Please write your code here.
     int a, b, c;
     cin >> a >> b >> c;
-    if (a > b && b < c){
+    if (a < b && b < c){
         cout << 1;
     } else {
         cout << 0;
