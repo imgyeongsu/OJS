cee2b5c42c01763ce74beb37064d4352a28d2e06
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-11-30 17:12:22 +0900

    Title: 장학금, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251130/장학금/scholarship.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int m, f;
+    cin >> m >> f;
+    if (m >= 90){
+        if (f >= 95){
+            cout << 100000;
+        } else if ( f>= 90) {
+            cout << 50000;
+        } else {
+            cout << 0;
+        }
+    } else {
+        cout << 0;
+    }
+    return 0;
+}
