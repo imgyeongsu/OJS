ee7a31bcb91c6d8cb071d47f35a1bf912f5c99a9
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-12-12 09:50:33 +0900

    Title: 문자열 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251212/문자열 출력하기/print-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str = "LeebrosCode\n";
+    int n;
+    cin >> n;
+    for (int i =0; i<n; i++){
+        cout << str;
+    }
+    return 0;
+}
