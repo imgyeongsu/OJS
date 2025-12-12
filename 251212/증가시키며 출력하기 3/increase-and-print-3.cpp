5bc9dbb2784547e9397bf4bf59009f30dff14273
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-12-12 09:31:20 +0900

    Title: 증가시키며 출력하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251212/증가시키며 출력하기 3/increase-and-print-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int start = 10;
+    int end = 26;
+    while (start <= end){
+        cout << start << ' ';
+        start += 2;
+    }
+    return 0;
+}
