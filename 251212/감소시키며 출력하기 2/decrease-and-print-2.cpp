7879e84d11d1c7f0b6caacd90e9411966c05598d
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-12-12 09:44:48 +0900

    Title: 감소시키며 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251212/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int start ,end;
+    start = 26;
+    end = 10;
+    while (start >= end){
+        cout << start << ' ';
+        start --;
+    }
+    return 0;
+}
