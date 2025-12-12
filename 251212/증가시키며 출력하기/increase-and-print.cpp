41133e32aca716a6531984ebb6f3d224c1a985d9
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-12-12 09:17:54 +0900

    Title: 증가시키며 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251212/증가시키며 출력하기/increase-and-print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    for (int i=5; i<= 17;){
+        cout << i << ' ';
+        i+=2;
+    }
+    return 0;
+}
