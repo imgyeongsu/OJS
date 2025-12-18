fd28bcb68eef5eef6653ba409159554d4cc02a91
imgyeongsu <115059777+imgyeongsu@users.noreply.github.com>
2025-12-18 17:56:51 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251218/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main() {
+    cin >> N;
+
+    // Please write your code here.
+
+    for (int i=0; i<N; i++){
+        cout << "12345^&*()_\n";
+    }
+
+    return 0;
+}
